321f927e07016f97b5066e4096b2f46234ec5ebc
nthnn <nathanneisip@gmail.com>
2023-11-18 15:45:44 +0800

    Updated version.

--- kernel/constants.asm
+++ kernel/constants.asm
@@ -1,4 +1,4 @@
-str_greet:          db 'Zync OS (Shell) v0.0.1', ENDL, ENDL, 0
+str_greet:          db 'Zync OS (Shell) v0.0.2', ENDL, ENDL, 0
 str_shell:          db '%> ', 0
 str_nline:          db '', ENDL, 0
 str_clear_cmd:      db 'clear', 0
